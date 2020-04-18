da4ad8b99ebcd9f27be969089a1769e3cbbc31fa
Viridian-Kailin <a.davisdonatelli@gmail.com>
2020-04-17 17:43:17 -0700

    Figured out how to pass a dict!

--- web-ui/breeding/views.py
+++ web-ui/breeding/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     #return HttpResponse("Greetings")
-    return render(request, 'breeding/punnett.html', {})
+    flowers = {'Cosmos': 1, 'Hyacinths': 2, 'Lilies': 3, 'Mums': 4, 'Pansies': 5, 'Roses': 6, 'Tulips': 7, 'Windflowers': 8}
+    return render(request, 'breeding/punnett.html', {'flowers': flowers})
