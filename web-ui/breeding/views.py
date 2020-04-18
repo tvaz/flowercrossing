7e18eeb7da87510682d96918f8677e90641b7893
Viridian-Kailin <a.davisdonatelli@gmail.com>
2020-04-17 18:01:50 -0700

    Setup for flower parent selection

--- web-ui/breeding/views.py
+++ web-ui/breeding/views.py
@@ -5,4 +5,5 @@ from django.shortcuts import render
 def index(request):
     #return HttpResponse("Greetings")
     flowers = {'Cosmos': 1, 'Hyacinths': 2, 'Lilies': 3, 'Mums': 4, 'Pansies': 5, 'Roses': 6, 'Tulips': 7, 'Windflowers': 8}
-    return render(request, 'breeding/punnett.html', {'flowers': flowers})
+    genes = {'RR': 100, 'Rr': 101, 'rr': 111, 'YY': 200, 'Yy': 201, 'yy': 211, 'WW': 300, 'Ww': 301, 'ww': 311, 'SS': 400, 'Ss': 401, 'ss': 411}
+    return render(request, 'breeding/punnett.html', {'flowers': flowers, 'genes': genes})
